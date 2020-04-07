9b1896c3f4f261393ff524f06af6b9a9f15da4dd
HectorEduardoFaciano <edu21.ef@gmail.com>
2020-04-07 18:18:21 -0300

    2. a)

--- tp2_1_1.cpp
+++ tp2_1_1.cpp
@@ -3,17 +3,17 @@
 #define N 4
 #define M 5
 
-int f,c;
-double mt[N][M];
-
 int main(){
-    for(f = 0; f<N; f++)
-    {
-        for(f = 0; f<M; f++)
-        { 
+    int f,c;
+    double mt[N][M];
+    
+    for(f = 0; f<N; f++){
+        for(c = 0; c<M; c++){ 
+            printf("[%d][%d] ", f, c);
             _cprintf("%lf ", mt[f][c]);
+            printf("\n");
         }
-        _cprintf("\n");
+        printf("\n\n");
     }
     getch();
     return 0;
