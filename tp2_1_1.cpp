c9a38ffc6eba67008722f174300b8b7db3ff65fd
HectorEduardoFaciano <edu21.ef@gmail.com>
2020-04-07 22:22:32 -0300

    2.d)

--- tp2_1_1.cpp
+++ tp2_1_1.cpp
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <conio.h>
+
 #define N 4
 #define M 5
 
+int f,c;
+double mt[N][M];
+
 int main(){
-    int f,c;
-    double mt[N][M];
     
     for(f = 0; f<N; f++){
         for(c = 0; c<M; c++){ 
